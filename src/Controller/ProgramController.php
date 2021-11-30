ae96f1eb4f3b06cd41ed0b5fbba3c1e49eb89d5c
uSeRnAmE <mariejosepg.celia@gmail.com>
2021-11-30 17:16:44 +0100

    create route program 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,11 +9,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/program", name="program_")
+ */
+
 class ProgramController extends AbstractController
 
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
 
     public function index(): Response
@@ -23,4 +28,16 @@ class ProgramController extends AbstractController
 
             'website' => 'Wild Séries',]);
     }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"} ,name="id")
+     *
+     */
+
+    public function show(int $id):Response
+    {
+        return $this->render('program/show.html.twig',['id'=>$id]);
+
+    }
+
 };
